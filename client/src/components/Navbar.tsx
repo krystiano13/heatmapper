af11b203495425504efad1f60a29a5c8a8c08aea
krystiano13 <crystianotv@gmail.com>
2024-06-10 16:21:14 +0200

    implement logging out

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,40 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export function Navbar() {
   const navigate = useNavigate();
+  const userContext = useContext(UserContext);
 
   return (
     <nav className="flex gap-8 items-center p-6 fixed w-full navbar-shadow">
       <NavLink className="text-white text-lg font-medium" to="/">
         Home
       </NavLink>
-      <NavLink className="text-white text-lg font-medium" to="/login">
-        Login
-      </NavLink>
-      <NavLink className="text-white text-lg font-medium" to="/register">
-        Register
-      </NavLink>
+      {!userContext?.user ? (
+        <>
+          <NavLink className="text-white text-lg font-medium" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="text-white text-lg font-medium" to="/register">
+            Register
+          </NavLink>
+        </>
+      ) : (
+        <>
+          <button
+            className="text-white text-lg font-medium"
+            id="logout"
+            onClick={() => {
+              userContext?.setUser(null);
+              localStorage.removeItem("refresh_token");
+              navigate("/login");
+            }}
+          >
+            Log Out
+          </button>
+        </>
+      )}
     </nav>
   );
 }
