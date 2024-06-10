898278f87cfa2d8b74b3b94d63f10abd2df9eafd
krystiano13 <crystianotv@gmail.com>
2024-06-10 13:29:32 +0200

    create routing with react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// views
+import { Home } from "./views/Home";
+import { Login } from "./views/Login";
+import { Register } from "./views/Register";
+
 export function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
